663d9a0c7e5725690148f2553d1ccb7a756408c0
harvey <harvey.ruqing.xu@gmail.com>
2016-04-01 12:00:09 -0400

    dropdown filter for index page

--- web/resources/script/eqhome.js
+++ web/resources/script/eqhome.js
@@ -1,15 +1,60 @@
-var table;
+var tdlist=[];
 $(document).ready(function () {
-
 //    $('.date').datepicker({
 //        autoclose: true,
 //        todayHighlight: true
 //        });
-//    $('.body').append($('<form>',{
-//        'style':'display:none',
-//        'id':'tempForm'
-//    }).append($("#tableStudy")));
-//    table=$("#tempForm").find('table');
+
+    $("#tableStudy").find('tbody').find('tr').each(function(){
+        tdlist.push($(this));
+    });
+    
+    //dropdown filter Study Status
+    $('#sltStudyStatus').change(function () {
+    var sponsorname=$('#sltSponsorName').val();
+    var status=$(this).val();
+    var tbody = $("<tbody>");
+    $.each(tdlist,function(index,value){
+        if(sponsorname==="All"&&status==="All"){
+             tbody.append($(this));
+        }
+        else if(sponsorname==="All"&&value.find('td:nth-child(6)').text().trim() === status){
+             tbody.append($(this));
+        }
+        else if(status==="All"&&value.find('td:nth-child(4)').text().trim() === sponsorname){
+            tbody.append($(this));
+        }
+        else if (value.find('td:nth-child(4)').text().trim() === sponsorname && value.find('td:nth-child(6)').text().trim() === status) {
+            tbody.append($(this));
+        }
+    });
+    $("#tableStudy").find("tbody").remove();
+    $("#tableStudy").append(tbody[0].outerHTML);
+});
+
+   //dropdown filter Sponsor Name
+    $('#sltSponsorName').change(function () {
+    var sponsorname=$(this).val();
+    var status=$('#sltStudyStatus').val();
+    var tbody = $("<tbody>");
+    $.each(tdlist,function(index,value){
+        if(sponsorname==="All"&&status==="All"){
+             tbody.append($(this));
+        }
+        else if(sponsorname==="All"&&value.find('td:nth-child(6)').text().trim() === status){
+             tbody.append($(this));
+        }
+        else if(status==="All"&&value.find('td:nth-child(4)').text().trim() === sponsorname){
+            tbody.append($(this));
+        }
+        else if (value.find('td:nth-child(4)').text().trim() === sponsorname && value.find('td:nth-child(6)').text().trim() === status) {
+            tbody.append($(this));
+        }
+    });
+    $("#tableStudy").find("tbody").remove();
+    $("#tableStudy").append(tbody[0].outerHTML);
+});
+
 
 });
 
@@ -21,18 +66,9 @@ function ToggleSubform(selector, form) {
     }
 }
 
-//dropdown filter
-function changeTable(status, sponsorname) {
-    debugger;
-    var tbody = $("<tbody>");
-    table.find('tbody').find('tr').each(function () {
-        if ($(this).find('td:nth-child(4)').text().trim() === sponsorname && $(this).find('td:nth-child(4)').text().trim() === sponsorname) {
-            tbody.append($(this));
-        }
-    });
-    $("#tableStudy").find("tbody").remove();
-    $("#tableStudy").append(tbody[0].outerHTML);
-}
+
+
+
 
 
 
