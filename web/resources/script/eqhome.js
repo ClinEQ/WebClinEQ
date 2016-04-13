b205c5e46cda3f10133ef06951930fa9d67efd01
harvey <harvey.ruqing.xu@gmail.com>
2016-04-13 17:30:17 -0400

    sponsor page

--- web/resources/script/eqhome.js
+++ web/resources/script/eqhome.js
@@ -47,55 +47,64 @@ $(document).ready(function () {
         todayHighlight: true
     });
     
-    $('#btnNextNewStudySponsor').on('click', function () {
-        var studySponPrimary = {
-            eqSponsorId: $("#inpEqhomeNSSEqSponId").val(),
-            eqSponsorName: $('#inpeqhomeNSSSponName').val(),
-            eqSponsorNameAbbr: $('#inpeqhomeNSSSponAB').val(),
-            eqSponsorType: $('#inpeqhomeNSSSponType').val(),
-            eqSponsorCate: $('#inpeqhomeNSSSponCate').val(),
-            eqSponsorAddress1: $('#inpeqhomeNSSAddress1').val(),
-            eqSponsorAddress2: $('#inpeqhomeNSSAddress2').val(),
-            eqSponsorCity: $('#inpeqhomeNSSCity').val(),
-            eqSponsorState: $('#inpeqhomeNSSState').val(),
-            eqSponsorZip: $('#inpeqhomeNSSZip').val(),
-            eqSponsorCountry: $('#inpeqhomeNSSCountry').val(),
-            eqSponsorPhone: $('#inpeqhomeNSSPhone').val(),
-            eqSponsorFax: $('#inpeqhomeNSSFax').val(),
-            eqSponsorUrl: $('#inpeqhomeNSSSponUrl').val(),
-            eqSponsorConName: $('#inpeqhomeNSSConName').val(),
-            eqSponsorEmail: $('#inpeqhomeNSSEmail').val(),
-            eqSponsorStatus: $('#inpeqhomeNSSStudyStatus').val(),
-            eqSponsorNotes: $('#txteqhomeNSSNote').val()
-        };
-        $.ajax({
-            url: "",
-            type: 'POST',
-            contentType: 'application/json;charset=utf-8;',
-            dataType: "json",
-            data: JSON.stringify(studySponPrimary),
-            success: function (data) {
-                if (data === "Success") {
-                    var sponsorUserList;
-                    $.each($("#tblNSSSponsorUserList").find('tbody').find('tr'), function (index, value) {
-                        sponsorUserList = {
-                            equserid: value.find('td:nth-child(1)').text(),
-                        }
-                    });
-                    $.ajax({
-                        url: "",
-                        type: 'POST',
-                        contentType: 'application/json;charset=utf-8;',
-                        dataType: "json",
-                        data: JSON.stringify(sponsorUserList),
-                        success: function (data) {
-                            location.href("");
-                        }
-                    });
-                }
-            }
-        });
+    $('#btnEqhomeNSMNext').on('click',function(){
+        var studyBisicInfo= JSON.stringify($("#formEqhomeNSMStudyBasicInfo").serializeArray());
+        localStorage.setItem("studyBisicInfo",studyBisicInfo);
+        window.location.href="../study/newStudySponsor";
     });
+    
+    
+    
+    
+//    $('#btnNewStudySponsorNext').on('click', function () {
+//        var studySponPrimary = {
+//            eqSponsorId: $("#inpEqhomeNSSEqSponId").val(),
+//            eqSponsorName: $('#inpeqhomeNSSSponName').val(),
+//            eqSponsorNameAbbr: $('#inpeqhomeNSSSponAB').val(),
+//            eqSponsorType: $('#inpeqhomeNSSSponType').val(),
+//            eqSponsorCate: $('#inpeqhomeNSSSponCate').val(),
+//            eqSponsorAddress1: $('#inpeqhomeNSSAddress1').val(),
+//            eqSponsorAddress2: $('#inpeqhomeNSSAddress2').val(),
+//            eqSponsorCity: $('#inpeqhomeNSSCity').val(),
+//            eqSponsorState: $('#inpeqhomeNSSState').val(),
+//            eqSponsorZip: $('#inpeqhomeNSSZip').val(),
+//            eqSponsorCountry: $('#inpeqhomeNSSCountry').val(),
+//            eqSponsorPhone: $('#inpeqhomeNSSPhone').val(),
+//            eqSponsorFax: $('#inpeqhomeNSSFax').val(),
+//            eqSponsorUrl: $('#inpeqhomeNSSSponUrl').val(),
+//            eqSponsorConName: $('#inpeqhomeNSSConName').val(),
+//            eqSponsorEmail: $('#inpeqhomeNSSEmail').val(),
+//            eqSponsorStatus: $('#inpeqhomeNSSStudyStatus').val(),
+//            eqSponsorNotes: $('#txteqhomeNSSNote').val()
+//        };
+//        $.ajax({
+//            url: "",
+//            type: 'POST',
+//            contentType: 'application/json;charset=utf-8;',
+//            dataType: "json",
+//            data: JSON.stringify(studySponPrimary),
+//            success: function (data) {
+//                if (data === "Success") {
+//                    var sponsorUserList;
+//                    $.each($("#tblNSSSponsorUserList").find('tbody').find('tr'), function (index, value) {
+//                        sponsorUserList = {
+//                            equserid: value.find('td:nth-child(1)').text(),
+//                        }
+//                    });
+//                    $.ajax({
+//                        url: "",
+//                        type: 'POST',
+//                        contentType: 'application/json;charset=utf-8;',
+//                        dataType: "json",
+//                        data: JSON.stringify(sponsorUserList),
+//                        success: function (data) {
+//                            location.href("");
+//                        }
+//                    });
+//                }
+//            }
+//        });
+//    });
 });
 
 
